34b813e69f6b84c44ec19fd7701df9dbcfb3abe0
alejandro <alejandro.gandia@gmail.com>
2017-01-30 16:27:42 +0100

    round decimals feature

--- js/services.js
+++ js/services.js
@@ -2,19 +2,19 @@ angular.module('calcService', [])
 	.factory('myFactory', function() {
 		var calcSum = function(firstOp, secOp) {
 			var total = parseInt(firstOp) + parseInt(secOp);
-			return total
+			return Math.round(total * 100) / 100
 		}
 		var calcRest = function(firstOp, secOp) {
 			var total2 = parseInt(firstOp) - parseInt(secOp);
-			return total2
+			return Math.round(total2 * 100) / 100
 		}
 		var calcMult = function(firstOp, secOp) {
 			var total3 = parseInt(firstOp) * parseInt(secOp);
-			return total3
+			return Math.round(total3 * 100) / 100
 		}
 		var calcDiv = function(firstOp, secOp) {
 			var total4 = parseInt(firstOp) / parseInt(secOp);
-			return total4
+			return Math.round(total4 * 100) / 100
 		}
 		return {
 			methodSum : calcSum,
